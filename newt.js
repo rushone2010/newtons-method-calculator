f77975f6632cd53501b03a7f2a53883f6532ac2f
Vladislav Reshetnyak <rushone2010@gmail.com>
2017-03-08 13:27:37 -0800

    Fixed some semicolons.

--- newt.js
+++ newt.js
@@ -1,11 +1,11 @@
 function funct(x) {
   //Get function formula.
   var funct_formula = document.getElementById("funct").value;
-  
+
   //Set x value to xval from newt function
   //and evaluate the expression. Pass back to newt function.
-  var scope = {x: x}
-  var funct_value = math.eval(funct_formula, scope); 
+  var scope = {x: x};
+  var funct_value = math.eval(funct_formula, scope);
 
   return funct_value;
 }
@@ -13,12 +13,12 @@ function funct(x) {
 function deriv(x) {
   //Get derivative formula.
   var deriv_formula = document.getElementById("deriv").value;
-  
+
   //Set x value to xval from newt function
   //and evaluate the expression. Pass back to newt function.
-  var scope = {x: x}
-  var deriv_value = math.eval(deriv_formula, scope); 
-  
+  var scope = {x: x};
+  var deriv_value = math.eval(deriv_formula, scope);
+
   return deriv_value;
 }
 
@@ -28,16 +28,16 @@ function newt() {
   var n = document.getElementById("nval").value;
 
   //Iterate through a loop based on the nval.
-  for (i = 0; i < n; i++) { 
+  for (i = 0; i < n; i++) {
     if (deriv(x) === 0) {
-      return x; 
+      return x;
     }
     else {
       //Newton'd Method Formula.
       x = x - funct(x)/deriv(x);
-    }    
+    }
   }
-  
+
   //Store the result in the finalVal.
   document.getElementById("finalVal").value = x;
 }
